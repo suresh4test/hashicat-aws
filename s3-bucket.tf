dcca30bfd7823e6443ccc546ba0ba7a773f80fdb
suresh4test <sureshservice1@gmail.com>
2021-06-29 15:46:45 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/suresh4-training/s3-bucket/aws"
+  source  = "https://app.terraform.io/app/suresh4-training/registry/modules/private/suresh4-training/s3-bucket/aws/2.5.0"
   version = "2.2.0"
   # insert required variables here
 }
